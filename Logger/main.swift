d6e2d0cc7d64c8a8602fb5372120d6fb23dd7f9b
Tore Nygaard-Jensen <tnj@trifork.com>
2019-04-22 06:05:44 +0200

    Initial Commit

---
+++ Logger/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Logger
+//
+//  Created by Tore Nygaard-Jensen on 22/04/2019.
+//  Copyright © 2019 TNJ. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
