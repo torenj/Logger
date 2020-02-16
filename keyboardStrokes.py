7f54b0b27ab4943618a74f3fae44804674c4b56c
Tore Nygaard-Jensen <tore.nj@gmail.com>
2020-02-16 14:57:07 +0100

    keyboard analysis. Trying to find the first and last of typed sequences to use for image_diff

--- keyboardStrokes.py
+++ keyboardStrokes.py
@@ -2,7 +2,7 @@ import os, fnmatch
 import re
 
 def collectWords(string):
-    symbols = ['{', '}', '(', ')', '[', ']', '"', '*','\t','\r','\n', ':', ','] # single-char keywords
+    symbols = ['{', '}', '[', ']', '"', '*','\t','\r','\n', ':', ','] # single-char keywords
     keywords = ['public', 'class', 'void', 'main', 'String', 'int','mouseDown']
     KEYWORDS = symbols + keywords
 
@@ -10,10 +10,9 @@ def collectWords(string):
     lexeme = ''
 
     for i,char in enumerate(string):
-        if char != white_space:
-            lexeme += char # adding a char each time
+        lexeme += char # adding a char each time
         if (i+1 < len(string)): # prevents error
-            if string[i+1] == white_space or string[i+1] in KEYWORDS or lexeme in KEYWORDS: # if next char == ' '
+            if string[i+1] in KEYWORDS or lexeme in KEYWORDS: # if next char == ' '
                 if lexeme != '':
                     print(lexeme.replace('\n', '<newline>').replace('\r', '<return>').replace('\t', '<tab>'))
                     lexeme = ''
@@ -21,14 +20,24 @@ def collectWords(string):
 listOfFiles = sorted(os.listdir('./input'))
 pattern = "*.jpg"
 characters = []
+markNext = False
+previousEntry = ""
 for entry in listOfFiles:
     if fnmatch.fnmatch(entry, pattern):
+        if markNext and not "mouseDown" in entry:
+            print("mark first:"+entry)
+            markNext = False
+        if not markNext and "mouseDown" in entry:
+            print("mark last:"+previousEntry)
+            markNext = True
+        print (entry)
+        previousEntry = entry
         char = ''
         if "mouseDown" in entry:
             char = 'mouseDown'
+            markNext = True
         else:
             char = re.search('{(.*)}', entry, re.IGNORECASE).group(1)
-        print (entry)
         characters.append(char)
 collectWords("".join(characters))
 
