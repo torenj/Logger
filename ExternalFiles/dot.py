6ab585cd2515119d330adf86b4087e7cb67d6d67
torenj <tore.nj@gmail.com>
2024-03-02 20:06:06 +0100

    refactored the dot.py script

--- ExternalFiles/dot.py
+++ ExternalFiles/dot.py
@@ -1,17 +1,19 @@
 import cv2
-import os, fnmatch
+import os
 import re
+import fnmatch
 
 listOfFiles = os.listdir('./input')
 pattern = "*mouseDown*.jpg"
-for entry in listOfFiles:
-    if fnmatch.fnmatch(entry, pattern):
-        x = re.search('{mouseDown\((.*),(.*)\)}', entry, re.IGNORECASE).group(1)
-        y = re.search('{mouseDown\((.*),(.*)\)}', entry, re.IGNORECASE).group(2)
-        image = cv2.imread('./input/'+entry)
-        height, width, channels = image.shape
-        xOnImage = int(x) * 2
-        yOnImage = int(y) * 2
-        cv2.circle(image,(int(xOnImage), int(height-yOnImage)), 25, (0,0,255),5)
-        cv2.imwrite('./output/'+entry,image)
+regex_pattern = '{mouseDown\\((.*),(.*)\\)}'
+
+for entry in filter(lambda file: fnmatch.fnmatch(file, pattern), listOfFiles):
+    match = re.search(regex_pattern, entry, re.IGNORECASE)
+    x, y = map(int, match.groups())
+    image_path = os.path.join('./input', entry)
+    image = cv2.imread(image_path)
+    height, _, _ = image.shape
+    cv2.circle(image, (x * 2, height - y * 2), 25, (0, 0, 255), 5)
+    cv2.imwrite(os.path.join('./output', entry), image)
+    
 print("dots processed")
